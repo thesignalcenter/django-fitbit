f3a9ea2880e8dbbdbbbaf83285667b62e6bd8b6a
Madeline Silva <madeline-silva@uiowa.edu>
2018-06-25 15:02:24 -0500

    Add get_intraday_data helper function from old fork, minor refactoring.

--- fitapp/tasks.py
+++ fitapp/tasks.py
@@ -1,15 +1,18 @@
+from datetime import timedelta
 import logging
 import random
+import sys
 
 from celery import shared_task
 from celery.exceptions import Ignore, Reject
 from dateutil import parser
 from django.core.cache import cache
+from django.utils.timezone import utc
 from django.db import transaction
-from fitbit.exceptions import HTTPBadRequest, HTTPTooManyRequests
+from fitbit.exceptions import HTTPBadRequest, HTTPTooManyRequests, HTTPUnauthorized
 
 from . import utils
-from .models import TimeSeriesData, TimeSeriesDataType, UserFitbit
+from .models import UserFitbit, TimeSeriesData, TimeSeriesDataType
 
 logger = logging.getLogger(__name__)
 LOCK_EXPIRE = 60 * 5  # Lock expires in 5 minutes
@@ -106,3 +109,89 @@ def get_time_series_data(self, fitbit_user, cat, resource, date=None):
     except Exception as e:
         logger.exception("Exception updating data: %s" % e)
         raise Reject(e, requeue=False)
+
+
+def get_intraday_data(fitbit_user, category, resource, date, tz_offset):
+    """Get the given Fitbit user's intraday data for a specified date and save it in the database.
+
+    Convert timestamps to UTC prior to saving.
+
+    The Fitbit API stipulates that intraday data can only be retrieved for one
+    day at a time.
+
+    Args:
+        fitbit_user (str): The Fitbit user ID from which to retrieve data.
+            Can come from UserFitbit.fitbit_user.
+        category (enum): One of the enumerated choices for TimeSeriesDataType.category.
+        resource (str): The resource as defined by the Fitbit API that we want to retrieve data for.
+        date (date): The day for which to retreive intraday data.
+        tz_offset (int): Offset in hours of the user's timezone to UTC.
+    """
+
+    # Ensure that the given category and resource are valid.
+    try:
+        time_series_data_type = TimeSeriesDataType.objects.get(category=category, resource=resource)
+    except TimeSeriesDataType.DoesNotExist:
+        logger.exception("The resource %s in category %s doesn't exist" %
+                         (resource, category))
+        raise Reject(sys.exc_info()[1], requeue=False)
+
+    # Ensure that the requested TimeSeriesDataType supports intraday data.
+    if not time_series_data_type.intraday_support:
+        logger.exception("The resource %s in category %s does not support "
+                         "intraday time series" % (resource, category))
+        raise Reject(sys.exc_info()[1], requeue=False)
+
+    # Create a lock so we don't try to run the same task multiple times
+    sdat = date.strftime('%Y-%m-%d')
+
+    fbusers = UserFitbit.objects.filter(fitbit_user=fitbit_user)
+    dates = {'base_date': date, 'period': '1d'}
+    try:
+        with transaction.atomic():
+            for fbuser in fbusers:
+                data = utils.get_fitbit_data(fbuser, time_series_data_type, return_all=True,
+                                             **dates)
+                resource_path = time_series_data_type.path().replace('/', '-')
+                key = resource_path + "-intraday"
+                if data[key]['datasetType'] != 'minute':
+                    logger.exception("The resource returned is not "
+                                     "minute-level data")
+                    raise Reject(sys.exc_info()[1], requeue=False)
+                intraday = data[key]['dataset']
+                logger.info("Date for intraday task: {}".format(date))
+                for minute in intraday:
+                    datetime = parser.parse(minute['time'], default=date)
+                    utc_datetime = datetime + timedelta(hours=tz_offset)
+                    utc_datetime = utc_datetime.replace(tzinfo=utc)
+                    value = minute['value']
+                    # Don't create unnecessary records
+                    if not utils.get_setting('FITAPP_SAVE_INTRADAY_ZERO_VALUES'):
+                        if int(float(value)) == 0:
+                            continue
+                    # Create new record or update existing
+                    tsd, created = TimeSeriesData.objects.get_or_create(
+                        user=fbuser.user, resource_type=time_series_data_type, date=utc_datetime,
+                        intraday=True)
+                    tsd.value = value
+                    tsd.save()
+            # Release the lock
+    except HTTPTooManyRequests:
+        # We have hit the rate limit for the user, retry when it's reset,
+        # according to the reply from the failing API call
+        e = sys.exc_info()[1]
+        logger.debug('Rate limit reached for user %s, will try again in %s seconds' %
+                     (fitbit_user, e.retry_after_secs))
+        raise get_intraday_data.retry(exc=e, countdown=e.retry_after_secs)
+    except HTTPBadRequest:
+        # If the resource is elevation or floors, we are just getting this
+        # error because the data doesn't exist for this user, so we can ignore
+        # the error
+        if not ('elevation' in resource or 'floors' in resource):
+            exc = sys.exc_info()[1]
+            logger.exception("Exception updating intraday data for user %s: %s" % (fitbit_user, exc))
+            raise Reject(exc, requeue=False)
+    except Exception:
+        exc = sys.exc_info()[1]
+        logger.exception("Exception updating data for user %s: %s" % (fitbit_user, exc))
+        raise Reject(exc, requeue=False)
